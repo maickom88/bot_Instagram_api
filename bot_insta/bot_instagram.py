e3d37e3dcc449ebfad0e5396e76198df6ab9c578
Michael Frank <maickomtutos@gmail.com>
2020-05-04 12:36:05 -0300

    add botweb

--- bot_insta/bot_instagram.py
+++ bot_insta/bot_instagram.py
@@ -11,7 +11,12 @@ class InstagramBot:
         self.username = username
         self.password = password
         self.usernameProfile = usernameProfile
-        self.bot =  webdriver.Chrome(executable_path=r'bot_insta/chromedriver.exe')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        self.bot = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     #[PT]Script para logar
     #[EN]Script for login
